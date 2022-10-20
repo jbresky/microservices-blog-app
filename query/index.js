72145693b4954fdb5f899113981ab2a593999023
jbresky <joaquin_bresky@hotmail.com>
2022-10-20 16:01:21 -0300

    using the query service

--- query/index.js
+++ query/index.js
@@ -1,16 +1,36 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser')
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/post', (req, res) => {
-
+app.get('/posts', (req, res) => {
+    res.send(posts)
 });
 
-app.post('/post', (req, res) => {
+const posts = {}
 
+app.post('/events', (req, res) => {
+    const { type, data } = req.body;
+
+    if(type === 'PostCreated'){
+        const { id, title } = data;
+
+        posts[id] = { id, title, comments: [] };
+    }
+
+    if(type === 'CommentCreated'){
+        const { id, content, postId } = data;
+
+        const post = posts[postId];
+        post.comments.push({ id, content });
+    }
+
+    res.send({});
+
+    console.log(posts);
 });
 
-app.listen(4002, () => console.log('Listening on 4002'))
+app.listen(4002, () => console.log('Listening on 4002'));
