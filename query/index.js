f8fa38e3722e25f61fc424e44436175d4f507a85
jbresky <joaquin_bresky@hotmail.com>
2022-10-20 21:51:41 -0300

    implementing event sync

--- query/index.js
+++ query/index.js
@@ -1,36 +1,65 @@
 const express = require('express');
 const cors = require('cors');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/posts', (req, res) => {
-    res.send(posts)
-});
-
 const posts = {}
 
-app.post('/events', (req, res) => {
-    const { type, data } = req.body;
-
-    if(type === 'PostCreated'){
+const handleEvent = (type, data) => {
+    if (type === 'PostCreated') {
         const { id, title } = data;
 
         posts[id] = { id, title, comments: [] };
     }
 
-    if(type === 'CommentCreated'){
-        const { id, content, postId } = data;
+    if (type === 'CommentCreated') {
+        const { id, content, postId, status } = data;
 
         const post = posts[postId];
-        post.comments.push({ id, content });
+        post.comments.push({ id, content, status });
     }
 
-    res.send({});
+    if (type === 'CommentUpdated') {
+        const { id, content, postId, status } = data;
 
-    console.log(posts);
+        const post = posts[postId];
+        const comment = post.comments.find(comment => {
+            return comment.id === id;
+        });
+
+        comment.status = status;
+        comment.content = content;
+    }
+}
+
+app.get('/posts', (req, res) => {
+    res.send(posts)
 });
 
-app.listen(4002, () => console.log('Listening on 4002'));
+app.post('/events', (req, res) => {
+    const { type, data } = req.body;
+
+    handleEvent(type, data)
+
+    res.send({});
+});
+
+app.listen(4002, async () => {
+    console.log('Listening on 4002');
+    try {
+        const res = await axios.get('http://localhost:4005/events');
+
+        for (let event of res.data) {
+            console.log('Processing event:', event.type);
+
+            handleEvent(event.type, event.data)
+        }
+    } catch (error) {
+        console.log(error.message);
+    }
+
+});
